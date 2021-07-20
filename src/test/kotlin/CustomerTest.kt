7d86f91df1bde8aa39444d0e338b8c6e4a540b96
Esdras Barreto <esdras.barreto@gmail.com>
2021-07-20 10:23:01 -0300

    Fixed order tests

--- src/test/kotlin/CustomerTest.kt
+++ src/test/kotlin/CustomerTest.kt
@@ -1,13 +1,16 @@
 import com.jetbrains.handson.httpapi.module
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import org.junit.FixMethodOrder
 import org.junit.Test
+import org.junit.runners.MethodSorters
 import kotlin.test.assertEquals
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class CustomerTest {
 
     @Test
-    fun testCustomerNotFound() {
+    fun a_testCustomerNotFound() {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/customer").apply {
                 assertEquals(HttpStatusCode.NotFound, response.status())
@@ -16,7 +19,7 @@ class CustomerTest {
     }
 
     @Test
-    fun testCustomerInsert() {
+    fun b_testCustomerInsert() {
         withTestApplication({ module(testing = true) }) {
             with(handleRequest(HttpMethod.Post, "/customer"){
                 addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
@@ -27,4 +30,14 @@ class CustomerTest {
             }
         }
     }
+
+    @Test
+    fun c_testCustomerFound() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/customer").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+        }
+    }
+
 }
